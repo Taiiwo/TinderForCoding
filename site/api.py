e8f2044a48c2281f82bda74420f7edfa909c437a
MooneyDev <mooneygames@live.com>
2015-07-29 11:15:52 +0100

    add submit form

--- site/api.py
+++ site/api.py
@@ -2,14 +2,20 @@ import pymongo
 import hashlib
 
 def register(req, email, passw, wd, be, fe, mad):
-   client = MongoClient('localhost', 27017)
-   db = client.coder8
-   users = db.users
-   data = {
-          "email": email,
-          "passw": sha512(passw),
-          
-          }
+    client = MongoClient('localhost', 27017)
+    db = client.coder8
+    users = db.users
+    data = {
+        "email": email,
+        "passw": sha512(passw),
+        "skills": {
+            "wd": wd,
+            "be": be,
+            "fe": fe,
+            "mad": mad
+        }
+    }
+    users.insert_one(data)
     return "%s, %s, %s, %s, %s, %s" % (email, passw, wd, be, fe, mad)
 
 def sha512(string):
